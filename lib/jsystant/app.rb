fe5fbe1c7d2fcada842ab1eea62abfd483b5747b
mathieul <mathieul@gmail.com>
2010-12-26 19:02:50 -0800

    moved sinatra file to their own directory

--- lib/jsystant/app.rb
+++ lib/jsystant/app.rb
@@ -5,6 +5,8 @@ module Jsystant
   class App < Thor
     include Thor::Actions
 
+    attr_accessor :project
+
     source_root File.expand_path("../../../templates", __FILE__)
     add_runtime_options!
     check_unknown_options!
@@ -14,9 +16,13 @@ module Jsystant
       :type => :boolean, :desc => "Destroy files"
 
     def create(project)
+      self.project = project
       self.behavior = :revoke if options[:destroy]
-      @project = project
       directory("create", project)
+      self.destination_root = project
+      copy_file("sinatra/app.rb", "app.rb")
+      template("sinatra/views/layout.haml.tt", "views/layout.haml")
+      template("sinatra/views/index.haml.tt", "views/index.haml")
     end
   end
 end
