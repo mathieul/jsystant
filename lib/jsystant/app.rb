cb26e904cc69f31897236930e371a1a7655813e5
mathieul <mathieul@gmail.com>
2010-12-26 20:06:51 -0800

    download requirejs when creating a new project

--- lib/jsystant/app.rb
+++ lib/jsystant/app.rb
@@ -11,7 +11,7 @@ module Jsystant
     add_runtime_options!
     check_unknown_options!
 
-    desc "create PROJECT", "create a new jsystant project"
+    desc "create PROJECT", "Create a new jsystant project"
     method_option :destroy, :aliases => '-d', :default => false,
       :type => :boolean, :desc => "Destroy files"
 
@@ -23,6 +23,27 @@ module Jsystant
       copy_file("sinatra/app.rb", "app.rb")
       template("sinatra/views/layout.haml.tt", "views/layout.haml")
       template("sinatra/views/index.haml.tt", "views/index.haml")
+      invoke("jsystant:download:requirejs", ["0.2.1", "1.4.4"])
+    end
+  end
+
+  class Download < Thor
+    include Thor::Actions
+
+    desc "requirejs REQUIREJS_VERSION [JQUERY_VERSION]", "Download require.js"
+    def requirejs(requirejs_version, jquery_version)
+      url = "http://requirejs.org/docs/release/#{requirejs_version}/minified/"
+      url += jquery_version.nil? ? "require.js" : "require-jquery-#{jquery_version}.js"
+      download_js(url, :vendor => false)
+    end
+
+    private
+
+    def download_js(url, opts = {:vendor => true})
+      destination = File.join("public", "javascripts")
+      destination = File.join(destination, "vendor") if opts[:vendor]
+      destination = File.join(destination, File.basename(url))
+      get(url, destination)
     end
   end
 end
