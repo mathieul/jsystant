2129856e9e18e3895dfe487a6b4398b1ced76517
mathieul <mathieul@gmail.com>
2010-12-30 18:20:52 -0800

    added specs for jQuery and require.js

--- spec/jsystant/app_spec.rb
+++ spec/jsystant/app_spec.rb
@@ -71,8 +71,33 @@ describe Jsystant::App do
       FakeWeb.register_uri(:get, 'http://requirejs.org/docs/release/1.2.3/minified/require.js',
         :body => 'content for require.js')
       silence(:stdout) { runner(:require => true).create("tomtom") }
-      (@path + "public/javascripts/require-1.2.3-min.js").should be_a_file
-      File.read(@path + "public/javascripts/require-1.2.3-min.js").should == 'content for require.js'
+      jpath = @path + "public/javascripts"
+      (jpath + "require-1.2.3-min.js").should be_a_file
+      File.read(jpath + "require-1.2.3-min.js").should == 'content for require.js'
+    end
+
+    it "installs jQuery when --jquery" do
+      FakeWeb.register_uri(:get, 'http://jquery.com/',
+        :body => '<html><body><p class="jq-version">Current Release: v1.4.5</p></body></html>')
+      FakeWeb.register_uri(:get, 'http://code.jquery.com/jquery-1.4.5.min.js',
+        :body => 'content for jQuery')
+      silence(:stdout) { runner(:jquery => true).create("tomtom") }
+      vpath = @path + "public/javascripts/vendor"
+      (vpath + "jquery-1.4.5-min.js").should be_a_file
+      File.read(vpath + "jquery-1.4.5-min.js").should == 'content for jQuery'
+    end
+
+    it "installs jQuery with integrated require.js support when --jquery && --require" do
+      FakeWeb.register_uri(:get, 'http://jquery.com/',
+        :body => '<html><body><p class="jq-version">Current Release: v0.1.2</p></body></html>')
+      FakeWeb.register_uri(:get, 'http://requirejs.org/docs/download.html',
+        :body => '<html><body><a href="" name="latest">Latest Release: 1.2.3</a></body></html>')
+      FakeWeb.register_uri(:get, 'http://requirejs.org/docs/release/1.2.3/minified/require-jquery-0.1.2.js',
+        :body => 'content for jQuery and require.js')
+      silence(:stdout) { runner(:jquery => true, :require => true).create("tomtom") }
+      jpath = @path + "public/javascripts"
+      (jpath + "require-1.2.3-jquery-0.1.2-min.js").should be_a_file
+      File.read(jpath + "require-1.2.3-jquery-0.1.2-min.js").should == 'content for jQuery and require.js'
     end
   end
 end
